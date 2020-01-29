9414575c1319bf82f8f29aca852f950eb81621a4
Gabriel Antiqueira <gmantiqueira@outlook.com>
2020-01-28 21:54:25 -0300

    Controller de login de admin

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
-import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
-routes.get('/users', UserController.index);
+routes.post('/session', SessionController.store);
 
 routes.use(authMiddleware);
 
