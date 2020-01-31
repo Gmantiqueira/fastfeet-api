757400252286386ed72c3d5452b326807d1021ee
Gabriel Antiqueira <gantiqueira@neoassist.com>
2020-01-31 13:30:30 -0300

    WIP | Delivery Model | Controller

---
+++ src/app/controllers/DeliveryController.js
@@ -0,0 +1,15 @@
+import * as Yup from 'yup';
+
+import Delivery from '../models/Delivery';
+
+class DeliveryController {
+  async index(req, res) {}
+
+  async post(req, res) {}
+
+  async update(req, res) {}
+
+  async delete(req, res) {}
+}
+
+export default new DeliveryController();
