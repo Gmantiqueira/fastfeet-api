106a17072c31a07349a1da5c702f1521f991eb74
Gabriel Antiqueira <gmantiqueira@outlook.com>
2020-02-01 20:58:48 -0300

    Delivery Controller

--- src/app/controllers/DeliveryController.js
+++ src/app/controllers/DeliveryController.js
@@ -1,15 +1,133 @@
 import * as Yup from 'yup';
 
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+
 import Delivery from '../models/Delivery';
+import Deliveryman from '../models/Deliveryman';
+import File from '../models/File';
+import Recipient from '../models/Recipient';
 
 class DeliveryController {
-  async index(req, res) {}
+  async index(req, res) {
+    const { page = 1 } = req.query;
 
-  async post(req, res) {}
+    const deliveries = await Delivery.findAll({
+      order: ['name'],
+      attributes: ['id', 'product', 'canceled_at', 'start_date', 'end_date'],
+      limit: 30,
+      offset: (page - 1) * 30,
+      include: [
+        {
+          model: Deliveryman,
+          as: 'deliveryman',
+          attributes: ['id', 'name', 'email'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['name', 'path', 'url'],
+            },
+          ],
+        },
+        {
+          model: File,
+          as: 'signature',
+          attributes: ['name', 'path', 'url'],
+        },
+        {
+          model: Recipient,
+          as: 'recipient',
+          attributes: [
+            'id',
+            'name',
+            'street',
+            'number',
+            'adjunct',
+            'city',
+            'state',
+            'country',
+            'zip_code',
+          ],
+        },
+      ],
+    });
 
-  async update(req, res) {}
+    return res.json(deliveries);
+  }
 
-  async delete(req, res) {}
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      product: Yup.string().required(),
+      deliveryman_id: Yup.string().required(),
+      recipient_id: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { product, deliveryman_id, recipient_id } = await Delivery.create(
+      req.body
+    );
+
+    return res.json({
+      product,
+      deliveryman_id,
+      recipient_id,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      recipient_id: Yup.string(),
+      deliveryman_id: Yup.string(),
+      signature_id: Yup.string(),
+      product: Yup.string(),
+      canceled_at: Yup.date(),
+      start_date: Yup.date(),
+      end_date: Yup.date(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { deliverymanId } = req.params;
+
+    const deliveryman = await Deliveryman.findByPk(deliverymanId);
+
+    const {
+      recipient_id,
+      deliveryman_id,
+      signature_id,
+      product,
+      canceled_at,
+      start_date,
+      end_date,
+    } = await deliveryman.update(req.body);
+
+    return res.json({
+      recipient_id,
+      deliveryman_id,
+      signature_id,
+      product,
+      canceled_at,
+      start_date,
+      end_date,
+    });
+  }
+
+  async delete(req, res) {
+    const { deliveryId } = req.params;
+
+    const data = await Delivery.delete({
+      where: {
+        delivery_id: deliveryId,
+      },
+    });
+
+    return res.json(data);
+  }
 }
 
 export default new DeliveryController();
