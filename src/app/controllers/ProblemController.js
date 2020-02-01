c21911c42b6c65e285dd007d3ed3893ac840c607
Gabriel Antiqueira <gmantiqueira@outlook.com>
2020-02-01 20:57:40 -0300

    Problem Model | Controller

---
+++ src/app/controllers/ProblemController.js
@@ -0,0 +1,104 @@
+import * as Yup from 'yup';
+
+import Problem from '../models/Problem';
+import Delivery from '../models/Delivery';
+import Deliveryman from '../models/Deliveryman';
+import File from '../models/File';
+import Recipient from '../models/Recipient';
+
+class ProblemController {
+  async index(req, res) {
+    const { deliveryId } = req.params;
+
+    const problems = await Problem.findAll({
+      where: { delivery_id: deliveryId, canceled_at: null },
+      order: ['date'],
+      attributes: ['description'],
+      include: [
+        {
+          model: Delivery,
+          as: 'delivery',
+          attributes: [
+            'id',
+            'product',
+            'canceled_at',
+            'start_date',
+            'end_date',
+          ],
+          include: [
+            {
+              model: Deliveryman,
+              as: 'deliveryman',
+              attributes: ['id', 'name', 'email'],
+              include: [
+                {
+                  model: File,
+                  as: 'avatar',
+                  attributes: ['name', 'path', 'url'],
+                },
+              ],
+            },
+            {
+              model: File,
+              as: 'signature',
+              attributes: ['name', 'path', 'url'],
+            },
+            {
+              model: Recipient,
+              as: 'recipient',
+              attributes: [
+                'id',
+                'name',
+                'street',
+                'number',
+                'adjunct',
+                'city',
+                'state',
+                'country',
+                'zip_code',
+              ],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(problems);
+  }
+
+  async store(req, res) {
+    const { deliveryId } = req.params;
+
+    const schema = Yup.object().shape({
+      description: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { description } = req.body;
+
+    await Problem.create({
+      delivery_id: deliveryId,
+      description,
+    });
+
+    return res.json({
+      deliveryId,
+      description,
+    });
+  }
+
+  async cancel(req, res) {
+    const { deliveryId } = req.params;
+
+    const delivery = await Delivery.findByPk(deliveryId);
+
+    await delivery.update({ canceled_at: new Date() });
+
+    res.json(delivery);
+  }
+}
+
+export default new ProblemController();
